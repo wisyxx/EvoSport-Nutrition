d8ab9396cbca6aa794c70c094c757c598c4a57ed
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2024-01-24 18:02:17 +0100

    Add landing page with styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './views/*/*.php'
-  ],
+  content: ['./views/*/*.php'],
   theme: {
     extend: {},
+    colors: {
+      text: 'var(--text)',
+      background: 'var(--background)',
+      primary: 'var(--primary)',
+      secondary: 'var(--secondary)',
+      accent: 'var(--accent)',
+      black: '#000'
+    },
   },
   plugins: [],
-}
+};
 
--- views/home/index.php
+++ views/home/index.php
@@ -1,1 +1,13 @@
-<h1 class=" text-cyan-300">From /</h1>
+<header class="flex flex-col sm:flex-row justify-center sm:justify-between items-center bg-background p-2 shadow-sm">
+    <div id="menu" class="flex flex-col justify-center mb-4 sm:mb-0 order-1 sm:order-none ml-0 sm:ml-10">
+        <div class="bg-black h-1 w-10 mb-2 rounded-sm"></div>
+        <div class="bg-black h-1 w-10 mb-2 rounded-sm"></div>
+        <div class="bg-black h-1 w-10 rounded-sm"></div>
+    </div>
+    <a href="/" class="order-0 sm:order-none">
+        <img src="build/img/EvoSportLogo.svg" alt="Companie logo">
+    </a>
+    <img class="order-2 sm:order-none mr-0 sm:mr-10" src="build/img/shopping-basket.svg" alt="Shopping basket logo button">
+</header>
+
+<?php $script = '<script src="build/js/app.js"></script>'; ?>
