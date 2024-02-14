f2d82cf76806c6f446d8c4413916cc6dae216e78
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2024-02-14 20:37:10 +0100

    Finish styling product page

--- views/products/product.php
+++ views/products/product.php
@@ -1,18 +1,23 @@
-<?php 
-    include_once __DIR__ . '/../templates/header.php';
+<?php
+include_once __DIR__ . '/../templates/header.php';
 ?>
 
 <div class="product">
-    <div class="product__info">
-        <h1 class="product__name"><?php echo $product->name ?></h1>
-        <img class="product__image" src="build/img/products/<?php echo $product->image ?>" alt="product image">
+    <div class="product__image">
+        <img src="build/img/products/<?php echo $product->image ?>" alt="product image">
     </div>
-    <aside class="product__actions">
+
+    <aside class="product__info">
+        <h1 class="product__name"><?php echo $product->name ?></h1>
         <p class="product__description"><?php echo $product->description ?></p>
         <p class="product__price"><?php echo $product->price ?>€</p>
+        <button class="button add">
+            <img src="build/img/shopping-basket.svg" alt="shopping basket">
+            Add
+        </button>
     </aside>
 </div>
 
 <?php
-    $script = '<script type="module" src="build/js/app.js"></script>';
+$script = '<script type="module" src="build/js/app.js"></script>';
 ?>
